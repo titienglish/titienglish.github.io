b707d0060a16e0eda73a50e6cc20f8a42c93b619
dangthien <thiennd.qti@vnpt.vn>
2025-06-18 14:41:25 +0700

    thine

---
+++ MyData/file.js
@@ -0,0 +1,14 @@
+﻿function openYouTubeChannel() {
+
+    var channelId = "@titienglish"; // Hoặc ID kênh nếu có
+    var appUrl = "vnd.youtube://" + channelId;
+    var webUrl = "https://www.youtube.com/" + channelId;
+
+    // Mở ứng dụng YouTube
+    window.location.href = appUrl;
+
+    // Nếu sau 1 giây không mở được ứng dụng, mở trên trình duyệt
+    setTimeout(function () {
+        window.open(webUrl, "_blank");
+    }, 1000);
+}
